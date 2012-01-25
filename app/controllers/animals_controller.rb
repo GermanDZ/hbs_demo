2f3ee925c6561976fc4aa4b2b83e31db303a1b21
GermanDZ <germ@ndz.com.ar>
2012-01-25 12:30:31 +0100

    multi view behavior moved to app controller

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -9,23 +9,4 @@ class AnimalsController < ApplicationController
     render_multi_view(action: 'animals/index')
   end
 
-  def from_json_view(options = {})
-    template = options[:template] || "#{params[:action]}.json.erb"
-    json = JSON::parse render_to_string(template, layout: false)
-    json.each_pair do |key, value|
-      instance_variable_set("@#{key}", value)
-    end
-  end
-
-  def render_multi_view(options = {})
-    action = options[:action] || params[:action]
-    layout = options[:layout] || true
-    respond_to do |wants|
-      wants.html do
-        from_json_view(template: "#{action}.json.erb")
-        render action, layout: layout
-      end
-      wants.json { render action }
-    end
-  end
 end
