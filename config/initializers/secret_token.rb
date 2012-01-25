292dc2ff0ff0de1637532539a64aaf4c3f2ebf5a
GermanDZ <germ@ndz.com.ar>
2012-01-25 10:31:35 +0100

    rails app skeleton added

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HbsDemo::Application.config.secret_token = '17b670b439f506e3ba0c4564110662a2e59d1ad0a8da0fa00d29e8286fbca522520f7f1bdcfc10c4b59bae77f8ffeac672b6c600da282ccb6a100b7ee50fa6e9'
